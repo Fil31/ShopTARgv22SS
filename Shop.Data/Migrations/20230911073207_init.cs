1a0a6b52286cfb6b3b40030650c195d7e7835deb
Fil31 <101242957+Fil31@users.noreply.github.com>
2023-11-12 12:18:30 +0200

    kindergarten working version + CRUD
    kindergarten working version + CRUD

--- Shop.Data/Migrations/20230911073207_init.cs
+++ Shop.Data/Migrations/20230911073207_init.cs
@@ -28,12 +28,37 @@ namespace Shop.Data.Migrations
                 {
                     table.PrimaryKey("PK_Spaceships", x => x.Id);
                 });
-        }
+
+            migrationBuilder.CreateTable(
+                name: "Kindergartens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GroupName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChildrenCount = table.Column<int>(type: "int", nullable: false),
+                    KindergartenName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Teacher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kindergartens", x => x.Id);
+                });
+
+            }
+
+
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "Spaceships");
+
+            migrationBuilder.DropTable(
+                name: "Kindergartens");
         }
+
+
     }
 }
